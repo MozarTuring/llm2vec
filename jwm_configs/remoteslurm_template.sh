4be3e3aa15a71038f9b26bb771588d7572108d1a
MozarTuring <marvin.maojw95@gmail.com>
2026-07-12 10:02:45 +0200

    v

---
+++ jwm_configs/remoteslurm_template.sh
@@ -0,0 +1,22 @@
+JWM_SERVER_NAME=
+export JWM_MODULES="Miniforge3 buildenv-gcccuda/12.4.1-gcc13.3.0"
+export JWM_ENVS="/proj/berzelius-aiics-real/users/x_jinma/conda_envs/llm2vec"
+export JWM_GPU_NUM=
+export JWM_NODES_NUM=
+export JWM_RUN_TIME=
+export CPUS_PER_TASK=$((8 * JWM_GPU_NUM))
+export MEM_PER_TASK="$((24 * JWM_GPU_NUM))G"
+export JWM_SLURM_FILE=slurm.sh
+
+module --force purge
+module load ${JWM_MODULES}
+
+
+if [ ! -d ${JWM_ENVS} ]; then
+
+conda create -p ${JWM_ENVS} python=3.10 -y
+
+fi
+conda activate ${JWM_ENVS}
+which python
+echo $PWD
