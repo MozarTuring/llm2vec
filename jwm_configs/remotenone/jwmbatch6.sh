9bee8b6aaa0af7ce668faaa90e4c53127f1ad755
MozarTuring <marvin.maojw95@gmail.com>
2026-07-31 10:35:30 +0200

    v

--- jwm_configs/remotenone/jwmbatch6.sh
+++ jwm_configs/remotenone/jwmbatch6.sh
@@ -1,7 +1,7 @@
 JWM_PYTHON="3.10"
 JWM_SERVER_NAME=greatrawr
 JWM_build_flashattn=
-JWM_NOTEBOOK=1
+JWM_NOTEBOOK=
 
 
 JWM_RUN_COMMAND="python experiments/run_layerwise_finetune.py train_configs/layerwise/MetaLlama3.1-mntp-layerwise.json"
--- jwm_configs/remotenone/remote_tmps/remote.sh
+++ jwm_configs/remotenone/remote_tmps/remote.sh
@@ -1,6 +1,6 @@
 set -e 
-export JWM_build_flashattn=
-export JWM_NOTEBOOK=1
-export JWM_RUN_COMMAND="python experiments/run_layerwise_finetune.py train_configs/layerwise/MetaLlama3.1-mntp-layerwise.json"
 export JWM_PYTHON="3.10"
+export JWM_RUN_COMMAND="python experiments/run_layerwise_finetune.py train_configs/layerwise/MetaLlama3.1-mntp-layerwise.json"
 export JWM_SERVER_NAME=greatrawr
+export JWM_build_flashattn=
+export JWM_NOTEBOOK=
