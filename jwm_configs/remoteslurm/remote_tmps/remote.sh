291fd3b9e00719a59ca87c95a4f93563beb51c3c
MozarTuring <marvin.maojw95@gmail.com>
2026-08-27 11:34:12 +0200

    v

--- jwm_configs/remoteslurm/remote_tmps/remote.sh
+++ jwm_configs/remoteslurm/remote_tmps/remote.sh
@@ -1,12 +1,12 @@
 set -e 
-export JWM_GPU_NUM=1
-export JWM_SLURM_FILE=slurm.sh
-export JWM_NODES_NUM=1
-export JWM_SERVER_NAME=berzeliusampere
-export JWM_RUN_COMMAND="python experiments/run_layerwise_finetune.py train_configs/layerwise/MetaLlama3.1-mntp-layerwise.json"
 export JWM_build_flashattn=
+export JWM_SLURM_FILE=slurm.sh
+export JWM_RUN_TIME="3-00:00:00"
+export JWM_SERVER_NAME=berzeliusampere
 export JWM_NOTEBOOK=
-export JWM_RUN_TIME="1-00:00:00"
+export JWM_GPU_NUM=1
+export JWM_NODES_NUM=1
+export JWM_RUN_COMMAND="python experiments/run_layerwise_finetune.py train_configs/layerwise/MetaLlama3.1-mntp-layerwise.json"
 
 require_env() {
 for var in "$@"; do
