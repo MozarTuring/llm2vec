c8f9d5e13b084630368cb5144ed3405f05287d6c
MozarTuring <marvin.maojw95@gmail.com>
2026-08-27 17:50:45 +0200

    v

--- jwm_configs/remotenone/remote_tmps/remote.sh
+++ jwm_configs/remotenone/remote_tmps/remote.sh
@@ -1,7 +1,7 @@
 set -e 
-export JWM_SERVER_NAME=greatrawr
+export JWM_NOTEBOOK=
 export CUDA_VISIBLE_DEVICES=1
 export JWM_RUN_COMMAND="python experiments/run_layerwise_finetune.py train_configs/layerwise/MetaLlama3.1-mntp-layerwise.json"
 export JWM_PYTHON="3.10"
-export JWM_NOTEBOOK=
+export JWM_SERVER_NAME=greatrawr
 export JWM_build_flashattn=
