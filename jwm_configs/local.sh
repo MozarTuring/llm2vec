c65479d137abc5ec6d326fb84e70c9d5af349d11
MozarTuring <marvin.maojw95@gmail.com>
2026-08-28 08:13:14 +0200

    v

---
+++ jwm_configs/local.sh
@@ -0,0 +1,9 @@
+
+source /Users/jinma63/Desktop/baidu/project_nogit/common_tools/common_tokens.sh
+
+
+# hf download frankniujc/saesae   --local-dir ./ --repo-type dataset   --include "Projects/sparse-retrieval/*"   --exclude "Projects/sparse-retrieval/checkpoints/*"   --exclude "Projects/sparse-retrieval/data/*"   --exclude "Projects/sparse-retrieval/experiments/*"   --exclude "Projects/sparse-retrieval/mteb_predictions/*"   --exclude "Projects/sparse-retrieval/results/*"
+
+# hf download frankniujc/saesae --local-dir ./ --repo-type dataset --include "Projects/splade/*" --exclude "Projects/splade/experiments/*" --exclude "Projects/splade/data/*"
+
+
