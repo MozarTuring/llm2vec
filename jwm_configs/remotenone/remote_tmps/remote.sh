cc3158126cb5b8d4b382511e1ecb7d80b0a97676
MozarTuring <marvin.maojw95@gmail.com>
2026-08-31 10:22:00 +0200

    v

--- jwm_configs/remotenone/remote_tmps/remote.sh
+++ jwm_configs/remotenone/remote_tmps/remote.sh
@@ -1,7 +1,7 @@
 set -e 
 export JWM_SERVER_NAME=greatrawr
-export CUDA_VISIBLE_DEVICES=1
 export JWM_build_flashattn=
+export CUDA_VISIBLE_DEVICES=1
 export JWM_RUN_COMMAND="python experiments/mteb_eval_layerwise.py \
 export JWM_PYTHON="3.10"
 export JWM_NOTEBOOK=
