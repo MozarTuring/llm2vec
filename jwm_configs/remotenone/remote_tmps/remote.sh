9399bcfd724613ce58bbdf59e7f1b7b11b9b8c5d
MozarTuring <marvin.maojw95@gmail.com>
2026-08-31 19:25:56 +0200

    v

--- jwm_configs/remotenone/remote_tmps/remote.sh
+++ jwm_configs/remotenone/remote_tmps/remote.sh
@@ -1,7 +1,7 @@
 set -e 
-export JWM_PYTHON="3.10"
-export CUDA_VISIBLE_DEVICES=1
-export JWM_RUN_COMMAND="python experiments/run_layerwise_finetune.py train_configs/layerwise/MetaLlama3.1-mntp-layerwise.json"
-export JWM_NOTEBOOK=
 export JWM_build_flashattn=
+export JWM_NOTEBOOK=
+export CUDA_VISIBLE_DEVICES=1
+export JWM_RUN_COMMAND="python experiments/mteb_eval_layerwise.py   --config output/layerwise/Meta-Llama-3.1-8B-msmarco-mntp-L0/MetaLlama3.1-mntp-layerwise.json   --trained_checkpoint_path output/layerwise/Meta-Llama-3.1-8B-msmarco-mntp-L0/checkpoint-3929   --query_top_k 40   --doc_top_k 400   --output_dir results   --cache_dir embedding_cache   --max_length 1024   --task_name SciFact"
+export JWM_PYTHON="3.10"
 export JWM_SERVER_NAME=greatrawr
