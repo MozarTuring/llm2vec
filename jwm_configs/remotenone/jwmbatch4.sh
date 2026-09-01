2e477a3c373cf57199c1f815217ee6436d6fc672
MozarTuring <marvin.maojw95@gmail.com>
2026-09-01 09:11:51 +0200

    v

--- jwm_configs/remotenone/jwmbatch4.sh
+++ jwm_configs/remotenone/jwmbatch4.sh
@@ -8,20 +8,20 @@ CUDA_VISIBLE_DEVICES=1
 
 # JWM_RUN_COMMAND="python experiments/reranker.py msmarco_hard_negatives.json --output reranked_hard_negatives.json --top_k 8"
 
-JWM_RUN_COMMAND="python experiments/run_layerwise_finetune.py train_configs/layerwise/MetaLlama3.1-mntp-layerwise.json"
+# JWM_RUN_COMMAND="python experiments/run_layerwise_finetune.py train_configs/layerwise/MetaLlama3.1-mntp-layerwise.json"
 
 # JWM_RUN_COMMAND="python experiments/diag_sae.py"
 
-# JWM_RUN_COMMAND="python experiments/mteb_eval_layerwise.py \
-#   --config output/layerwise/Meta-Llama-3.1-8B-msmarco-mntp-L0/MetaLlama3.1-mntp-layerwise.json \
-#   --trained_checkpoint_path output/layerwise/Meta-Llama-3.1-8B-msmarco-mntp-L0/checkpoint-3929 \
-#   --query_top_k 40 \
-#   --doc_top_k 400 \
-#   --output_dir results \
-#   --cache_dir embedding_cache \
-#   --max_length 1024 \
-#   --task_name SciFact"
-#
+JWM_RUN_COMMAND="python experiments/mteb_eval_layerwise.py \
+  --config output/layerwise/Meta-Llama-3.1-8B-msmarco-mntp-L0/MetaLlama3.1-mntp-layerwise.json \
+  --trained_checkpoint_path output/layerwise/Meta-Llama-3.1-8B-msmarco-mntp-L0/checkpoint-3929 \
+  --query_top_k 40 \
+  --doc_top_k 400 \
+  --output_dir results \
+  --cache_dir embedding_cache \
+  --max_length 1024 \
+  --task_name SciFact"
+
 
 
 
