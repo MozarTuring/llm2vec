fc5b10266df18bce43fb94b07ebe38e7217021e1
MozarTuring <marvin.maojw95@gmail.com>
2026-09-21 18:30:59 +0200

    v

--- pkq_configs/remote/remote_tmps/remote.sh
+++ pkq_configs/remote/remote_tmps/remote.sh
@@ -8,7 +8,7 @@ export PKQ_DATA_DIR=/nobackup/proj/disk/naiss2026-3-658/personal/jinma63/project
 cd ${RUN_DIR_HOME}/project_remote_pkq/${RUN_PROJ}
 export PKQ_PYTHON="3.10"
 export PKQ_SERVER_NAME=arrhenius
-export PKQ_GPU_NUM=4
+export PKQ_GPU_NUM=2
 export PKQ_NODES_NUM=1
 export PKQ_RUN_TIME="1-00:00:00"
 export PKQ_SLURM_FILE=slurm.sh
@@ -18,4 +18,4 @@ export PKQ_INTERACTIVE=
 export MEM_PER_TASK="$((80 * PKQ_GPU_NUM))G"
 export CPUS_PER_TASK=$((8 * PKQ_GPU_NUM))
 export CUDA_VISIBLE_DEVICES=1
-export PKQ_RUN_COMMAND="torchrun --nproc_per_node=${PKQ_GPU_NUM} experiments/run_layerwise_finetune.py     --config train_configs/layerwise/MetaLlama3.1-mntp-layerwise.json     --hard_negatives_file ${PKQ_DATA_DIR}/reranker_parts/"
+export PKQ_RUN_COMMAND="python experiments/run_mntp.py train_configs/mntp/MetaLlama3.1-msmarco.json"
